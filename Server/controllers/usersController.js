a938cdafe1bd91434e14c31bd19848fed7272079
Jmajane <jordan.majane@outlook.com>
2022-07-14 12:04:56 -0400

    usersController updated

--- Server/controllers/usersController.js
+++ Server/controllers/usersController.js
@@ -1,5 +1,16 @@
+const User = require("../model/userModel")
+const brcypt = require("bcrypt")
+
 module.exports.register = (req, res, next) => {
     const {username, email, pasword} = req.body;
-    const unsernameCheck = await
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck) {
+        return res.json({msg: "Username already used", status: false});
+    }
+    const emailCheck = await User.findOne({ email })
+    if (emailCheck) {
+        return res.json({msg: "Email already used", status: false});
+    }
+    
 };
 
