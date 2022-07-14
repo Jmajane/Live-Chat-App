8d2f744161d5ab41aaa9101e31c90f60203632d1
Jmajane <jordan.majane@outlook.com>
2022-07-14 11:49:47 -0400

    username added in schema

--- Server/model/userModel.js
+++ Server/model/userModel.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema ({
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true,
+    },
     
 })
